88d8c63d94248d597cd04633e5da1111db88f041
Josh McKenzie <josh.mckenzie@datastax.com>
2015-07-17 17:29:53 -0400

    remove unused import

--- test/unit/org/apache/cassandra/SchemaLoader.java
+++ test/unit/org/apache/cassandra/SchemaLoader.java
@@ -19,8 +19,6 @@ package org.apache.cassandra;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 import org.junit.After;
