044ae993e1237ce4987ffcd88fd3172bb7375f6c
Josh McKenzie <joshua.mckenzie@gmail.com>
2019-05-29 11:43:54 -0400

    tidy up interface and includes

--- src/java/org/apache/cassandra/db/commitlog/CommitLogSegmentAllocator.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLogSegmentAllocator.java
@@ -19,30 +19,13 @@
 package org.apache.cassandra.db.commitlog;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.concurrent.*;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.util.concurrent.RateLimiter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.db.Mutation;
-import org.apache.cassandra.db.commitlog.CommitLogSegment.CDCState;
-import org.apache.cassandra.exceptions.CDCWriteException;
-import org.apache.cassandra.io.util.FileUtils;
-import org.apache.cassandra.utils.DirectorySizeCalculator;
-import org.apache.cassandra.utils.NoSpamLogger;
 
 public interface CommitLogSegmentAllocator
 {
-    public void start();
-    public void shutdown();
+    void start();
+    void shutdown();
 
     /**
      * Indicates that a segment file has been flushed and is no longer needed. This generally performs blocking disk
@@ -51,22 +34,22 @@ public interface CommitLogSegmentAllocator
      * @param segment segment to be discarded
      * @param delete  whether or not the segment is safe to be deleted.
      */
-    public void discard(CommitLogSegment segment, boolean delete);
+    void discard(CommitLogSegment segment, boolean delete);
 
     /**
      * Allocate a segment. This is always expected to succeed so should throw some form of exception on failure to
      * allocate; if you can't allocate a CLS, you can no longer write and the node is in a bad state.
      */
-    public CommitLogSegment.Allocation allocate(Mutation mutation, int size);
+    CommitLogSegment.Allocation allocate(Mutation mutation, int size);
 
     /**
      * Hook to allow segment managers to track state surrounding creation of new segments. This takes place on a segment
      * management thread instead of the calling thread context.
      */
-    public CommitLogSegment createSegment();
+    CommitLogSegment createSegment();
 
     /**
      * When segments complete replay, the allocator has a hook to take action at that time.
      */
-    public void handleReplayedSegment(final File file);
+    void handleReplayedSegment(final File file);
 }
